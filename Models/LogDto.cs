ab612b27d593c8d99803c7650f10053303b3f546
Ray Clanan <rclanan@utopianconcept.com>
2016-12-08 09:06:22 -0500

    Remove unused directives

--- Models/LogDto.cs
+++ Models/LogDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AutoRenter.API.Models
+﻿namespace AutoRenter.API.Models
 {
     public class LogDto
     {
