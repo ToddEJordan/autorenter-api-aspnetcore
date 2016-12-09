c1d1efa45e404f35cf4232b0f6e7a351bc9497f7
Ray Clanan <rclanan@utopianconcept.com>
2016-12-09 16:55:54 -0500

    Remove unused code

--- Data/AutoRenterContext.cs
+++ Data/AutoRenterContext.cs
@@ -11,22 +11,5 @@ namespace AutoRenter.API.Data
 
         public DbSet<Location> Locations { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            /*foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
-                relationship.DeleteBehavior = DeleteBehavior.Restrict;
-
-            modelBuilder.Entity<Location>()
-                .ToTable("Location");
-
-            modelBuilder.Entity<Vehicle>()
-                .ToTable("Vehicles");
-
-            modelBuilder.Entity<Vehicle>()
-                .HasOne(a => a.Location)
-                .WithMany(s => s.Vehicles)
-                .HasForeignKey(a => a.LocationId);*/
-        }
     }
 }
