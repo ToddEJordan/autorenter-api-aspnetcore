8f6fe24b953fbb04aed5fdbb9cc399c75c68b3d7
Pascucci <cpascucci@fusionalliance.com>
2017-03-01 15:42:20 -0500

    Modified LookupDataController to get data from AutoRenterContext.

--- Features/LookupData/LookupDataController.cs
+++ Features/LookupData/LookupDataController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using AutoRenter.API.Data;
 using AutoRenter.API.Domain;
 using AutoRenter.API.Services;
 using MediatR;
@@ -14,25 +16,16 @@ namespace AutoRenter.API.Features.LookupData
     [Route("api/lookup-data")]
     public class LookupDataController : Controller
     {
+        private readonly AutoRenterContext _context;
         private readonly IMediator _mediator;
         private readonly IResponseConverter _responseConverter;
 
-        public LookupDataController(IResponseConverter responseConverter, IMediator mediator)
+        public LookupDataController(AutoRenterContext context, IResponseConverter responseConverter, IMediator mediator)
         {
+            _context = context;
             _responseConverter = responseConverter;
             _mediator = mediator;
         }
-
-        // [HttpGet]
-        // public async Task<IActionResult> Get()
-        // {
-        //     var query = new GetAll.Query();
-        //     var model = await _mediator.SendAsync(query);
-        //     var formattedResult = _responseConverter.Convert(model);
-
-        //     Response.Headers.Add("x-total-count", model.LookupData.ToString());
-        //     return Ok(formattedResult);
-        // }
 
         [HttpGet]
         public dynamic Get()
@@ -45,74 +38,27 @@ namespace AutoRenter.API.Features.LookupData
             return Ok(formattedResult);
         }
 
-        // private Domain.LookupData GetData(Boolean makes, Boolean models, Boolean states)
-        // {
-        //     var lookupData = new Domain.LookupData();
-        //     if (makes) {
-        //         ICollection<Make> makesData = new List<Make> {
-        //             new Make{Id = "tsl", Name = "Tesla"},
-        //             new Make{Id = "che", Name = "Chevrolet"},
-        //             new Make{Id = "frd", Name = "Ford"}
-        //         };
-        //         lookupData.Makes = makesData;
-        //     }
-
-        //     if (models) {
-        //         ICollection<Model> modelsData = new List<Model> {
-        //             new Model{Id = "tms", Name = "Model S"},
-        //             new Model{Id = "tmx", Name = "Model X"},
-        //             new Model{Id = "cvt", Name = "Corvette"},
-        //             new Model{Id = "fxp", Name = "Explorer"},
-        //             new Model{Id = "fta", Name = "Taurus"}
-        //         };
-        //         lookupData.Models = modelsData;
-        //     }
-
-        //     if (states) {
-        //         ICollection<State> statesData = new List<State> {
-        //             new State{StateCode = "AZ", Name = "Arizona"},
-        //             new State{StateCode = "CA", Name = "California"},
-        //             new State{StateCode = "HI", Name = "Hawaii"},
-        //             new State{StateCode = "IN", Name = "Indiana"},
-        //             new State{StateCode = "WA", Name = "Washington"}
-        //         };
-        //         lookupData.States = statesData;
-        //     }
-
-        //     return lookupData;
-        // }
-
         private Dictionary<string,object> GetData(IQueryCollection lookupTypes)
         {
             Dictionary<string,object> lookupData = new Dictionary<string, object>();
             if (lookupTypes.ContainsKey("makes")) {
-                ICollection<Make> makesData = new List<Make> {
-                    new Make{Id = "tsl", Name = "Tesla"},
-                    new Make{Id = "che", Name = "Chevrolet"},
-                    new Make{Id = "frd", Name = "Ford"}
-                };
+                ICollection<Make> makesData = _context.Makes.AsEnumerable()
+                    .OrderBy(s => s.Id)
+                    .ToList();
                 lookupData.Add("makes", makesData);
             }
 
             if (lookupTypes.ContainsKey("models")) {
-                ICollection<Model> modelsData = new List<Model> {
-                    new Model{Id = "tms", Name = "Model S"},
-                    new Model{Id = "tmx", Name = "Model X"},
-                    new Model{Id = "cvt", Name = "Corvette"},
-                    new Model{Id = "fxp", Name = "Explorer"},
-                    new Model{Id = "fta", Name = "Taurus"}
-                };
+                ICollection<Model> modelsData = _context.Models.AsEnumerable()
+                    .OrderBy(s => s.Id)
+                    .ToList();
                 lookupData.Add("models", modelsData);
             }
 
             if (lookupTypes.ContainsKey("states")) {
-                ICollection<State> statesData = new List<State> {
-                    new State{StateCode = "AZ", Name = "Arizona"},
-                    new State{StateCode = "CA", Name = "California"},
-                    new State{StateCode = "HI", Name = "Hawaii"},
-                    new State{StateCode = "IN", Name = "Indiana"},
-                    new State{StateCode = "WA", Name = "Washington"}
-                };
+                ICollection<State> statesData = _context.States.AsEnumerable()
+                    .OrderBy(s => s.StateCode)
+                    .ToList();
                 lookupData.Add("states", statesData);
             }
 
