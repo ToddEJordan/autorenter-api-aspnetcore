960d732dedf88d9fc5de9f365d14886613a1da8a
BWortman <bwortman@fusionalliance.com>
2017-03-07 18:27:50 -0500

    Very minor refactoring.

--- AutoRenter.Api/AppSettings.cs
+++ AutoRenter.Api/AppSettings.cs
@@ -6,15 +6,4 @@
 
         public virtual TokenSettings TokenSettings { get; set; }
     }
-
-    public class TokenSettings
-    {
-        public virtual string Issuer { get; set; }
-
-        public virtual string Audience { get; set; }
-
-        public virtual int ExpirationMinutes { get; set; }
-
-        public virtual string Secret { get; set; }
-    }
 }
---
+++ AutoRenter.Api/TokenSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoRenter.Api
+{
+    public class TokenSettings
+    {
+        public virtual string Issuer { get; set; }
+
+        public virtual string Audience { get; set; }
+
+        public virtual int ExpirationMinutes { get; set; }
+
+        public virtual string Secret { get; set; }
+    }
+}
