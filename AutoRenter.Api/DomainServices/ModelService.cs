72adb77a85e8285faa1da85e5e57a185e750673c
Jeremiah Loveland <jloveland@fusionalliance.com>
2017-05-16 09:12:34 -0400

    Added MakeService and ModelService.

---
+++ AutoRenter.Api/DomainServices/ModelService.cs
@@ -0,0 +1,109 @@
+﻿using AutoRenter.Api.Data;
+using AutoRenter.Api.DomainInterfaces;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoRenter.Api.Commands;
+using AutoRenter.Api.Data;
+using AutoRenter.Api.Domain;
+using AutoRenter.Api.DomainInterfaces;
+
+namespace AutoRenter.Api.DomainServices
+{
+    public class ModelService : IModelService, IDomainService, IDisposable
+    {
+        private bool disposed = false;
+        private readonly AutoRenterContext context;
+        private readonly IValidationService validationService;
+        public ModelService(AutoRenterContext context, IValidationService validationService)
+        {
+            this.context = context;
+            this.validationService = validationService;
+        }
+
+        public async Task<ResultCode> Delete(Guid id)
+        {
+            var getCommand = CommandFactory<Model>.CreateGetCommand(context);
+            var getResult = await getCommand.Execute(id);
+
+            if (getResult.ResultCode == ResultCode.NotFound)
+            {
+                return ResultCode.NotFound;
+            }
+
+            if (!await validationService.IsValidDelete(getResult.Data))
+            {
+                return ResultCode.BadRequest;
+            }
+
+            var command = CommandFactory<Model>.CreateDeleteCommand(context);
+            return await command.Execute(getResult.Data);
+        }
+
+        public async Task<Result<Model>> Get(Guid id)
+        {
+            var command = CommandFactory<Model>.CreateGetCommand(context);
+            return await command.Execute(id);
+        }
+
+        public async Task<Result<Model>> Get(string id)
+        {
+            var Model = context.Models.FirstOrDefault(x => x.ExternalId == id);
+            if (Model != null)
+            {
+                return new Result<Model>(ResultCode.Success, Model);
+            }
+
+            return new Result<Model>(ResultCode.NotFound);
+        }
+
+        public async Task<Result<IEnumerable<Model>>> GetAll()
+        {
+            var command = CommandFactory<Model>.CreateGetAllCommand(context);
+            return await command.Execute();
+        }
+
+        public async Task<Result<Guid>> Insert(Model Model)
+        {
+            if (!await validationService.IsValidInsert(Model))
+            {
+                return new Result<Guid>(ResultCode.BadRequest);
+            }
+
+            var command = CommandFactory<Model>.CreateInsertCommand(context);
+            return await command.Execute(Model);
+        }
+
+        public async Task<Result<Guid>> Update(Model Model)
+        {
+            if (!await validationService.IsValidUpdate(Model))
+            {
+                return new Result<Guid>(ResultCode.BadRequest);
+            }
+
+            var command = CommandFactory<Model>.CreateUpdateCommand(context);
+            return await command.Execute(Model);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                    disposed = true;
+                }
+            }
+        }
+    }
+}
---
+++ AutoRenter.Api/Models/MakeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoRenter.Api.Models
+{
+    public class MakeModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
