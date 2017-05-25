47e7f8734ef116cf2f005940c8d1a0fd583be78d
Jeremiah Loveland <jloveland@fusionalliance.com>
2017-05-25 17:17:37 -0400

    Added InfoController and Unit Tests to return AppInfo.

---
+++ AutoRenter.Api.Tests/InfoControllerTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using AutoRenter.Api.Controllers;
+using AutoRenter.Api.Models;
+
+namespace AutoRenter.Api.Tests
+{
+    public class InfoControllerTests
+    {
+        [Fact]
+        public async Task Get_ReturnsInfo()
+        {
+            // arrange
+            var sut = new InfoController();
+
+            // act
+            var response = await sut.Get();
+            var result = response as OkObjectResult;
+
+            // assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsTitle()
+        {
+            // arrange
+            var sut = new InfoController();
+
+            // act
+            var response = await sut.Get();
+            var result = response as OkObjectResult;
+            var resultValue = (Dictionary<string, object>)result.Value;
+
+            var infoResult = (ApiInfoModel)resultValue["data"];
+
+            // assert
+            Assert.True(infoResult.Title.Length > 0);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsEnvironment()
+        {
+            // arrange
+            var sut = new InfoController();
+
+            // act
+            var response = await sut.Get();
+            var result = response as OkObjectResult;
+            var resultValue = (Dictionary<string, object>)result.Value;
+
+            var infoResult = (ApiInfoModel)resultValue["data"];
+
+            // assert
+            Assert.True(infoResult.Environment.Length > 0);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsVersion()
+        {
+            // arrange
+            var sut = new InfoController();
+
+            // act
+            var response = await sut.Get();
+            var result = response as OkObjectResult;
+            var resultValue = (Dictionary<string, object>)result.Value;
+
+            var infoResult = (ApiInfoModel)resultValue["data"];
+
+            // assert
+            Assert.True(infoResult.Version.Length > 0);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsBuild()
+        {
+            // arrange
+            var sut = new InfoController();
+
+            // act
+            var response = await sut.Get();
+            var result = response as OkObjectResult;
+            var resultValue = (Dictionary<string, object>)result.Value;
+
+            var infoResult = (ApiInfoModel)resultValue["data"];
+
+            // assert
+            Assert.True(infoResult.Build.Length > 0);
+        }
+    }
+}
---
+++ AutoRenter.Api/Controllers/InfoController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using AutoRenter.Api.Models;
+
+namespace AutoRenter.Api.Controllers
+{
+    [Route("api/")]
+    public class InfoController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get()
+        {
+            var apiInfo = GetApiInfo();
+            var formattedResult = new Dictionary<string, object>
+            {
+                { "data", apiInfo }
+            };
+            return Ok(formattedResult);
+        }
+
+        private ApiInfoModel GetApiInfo()
+        {
+            return new ApiInfoModel
+            {
+                Title = "AutoRenter API",
+                Environment = GetEnvironment(),
+                Version = GetVersion(),
+                Build = GetBuild()
+            };
+        }
+
+        private string GetEnvironment()
+        {
+            var version = Microsoft.Extensions.PlatformAbstractions
+                .PlatformServices.Default.Application.RuntimeFramework
+                .Version;
+
+            var name = Microsoft.Extensions.PlatformAbstractions
+                .PlatformServices.Default.Application.RuntimeFramework
+                .FullName;
+
+            return $"{name} {version}";
+        }
+
+        private string GetVersion()
+        {
+            return Microsoft.Extensions.PlatformAbstractions
+                .PlatformServices.Default.Application
+                .ApplicationVersion;
+        }
+
+        private string GetBuild()
+        {
+            return "not-implemented";
+        }
+    }
+}
