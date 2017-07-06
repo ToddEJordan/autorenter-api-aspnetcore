cec535227ea6ffe2613f8981aecd74a483eeac96
Jeremiah Loveland <jloveland@fusionalliance.com>
2017-07-06 10:15:57 -0400

    Updated to dynamically set the host Urls based on the environment.

--- AutoRenter.Api/Program.cs
+++ AutoRenter.Api/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -8,20 +9,56 @@ namespace AutoRenter.Api
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
+            var config = BuildConfiguration(args);
+            var webHost = BuildWebHost(config);
+            webHost.Run();
+        }
+
+        private static IWebHost BuildWebHost(IConfigurationRoot config)
+        {
+            var envUrls = Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+            if (!string.IsNullOrWhiteSpace(envUrls))
+            {
+                return BuildHostWithUrls(config, envUrls);
+            }
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            {
+                return BuildHostWithUrls(config, "http://*:3000");
+            }
+
+            return BuildHost(config);
+        }
+
+        private static IConfigurationRoot BuildConfiguration(string[] args)
+        {
+            return new ConfigurationBuilder()
                 .AddCommandLine(args)
                 .AddEnvironmentVariables("ASPNETCORE_")
                 .Build();
+        }
 
-            var host = new WebHostBuilder()
+        private static IWebHost BuildHostWithUrls(IConfigurationRoot config, string urls)
+        {
+            return new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls(urls)
+                .Build();
+        }
+
+        private static IWebHost BuildHost(IConfigurationRoot config)
+        {
+            return new WebHostBuilder()
                 .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
         }
     }
 }
